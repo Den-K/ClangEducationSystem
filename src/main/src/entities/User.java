5c5bde9ac2a5e5d7b0f0ba2fe365937286b36de4
Den-K <denis.kobernik@gmail.com>
2016-05-24 16:59:24 +0300

    user entity

--- src/main/src/entities/User.java
+++ src/main/src/entities/User.java
@@ -2,9 +2,41 @@ package entities;
 
 public class User {
 
+    private int id;
+
     private String firstName;
 
     private String lastName;
+
+    public User(int id, String firstName, String lastName, String email)
+    {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+    }
+
+    private String email;
+
+    public int getId()
+    {
+        return id;
+    }
+
+    public void setId(int id)
+    {
+        this.id = id;
+    }
+
+    public String getEmail()
+    {
+        return email;
+    }
+
+    public void setEmail(String email)
+    {
+        this.email = email;
+    }
 
     public String getFirstName() {
         return firstName;
@@ -23,10 +55,13 @@ public class User {
     }
 
     @Override
-    public String toString() {
+    public String toString()
+    {
         return "User{" +
-                "firstName='" + firstName + '\'' +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
                 ", lastName='" + lastName + '\'' +
+                ", email='" + email + '\'' +
                 '}';
     }
 }
